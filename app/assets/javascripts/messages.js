34e824d3ee104ee61dc8edea440d4ce92a4cacdf
gotanda555 <crok2558@gmail.com>
2019-10-13 00:51:23 +0900

    完成（仮）

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -47,4 +47,92 @@ $(document).on('turbolinks:load', function(){
     $('.all__chat__footer__class__send').prop('disabled', false);
   })
 })
+});
+
+
+$(function() {
+
+  var buildMessageHTML = function(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    if (content && img) {
+      //data-idが反映されるようにしている
+      var html = '<div class="all__chat__main__class" data-id=' + message.id + '>' +
+        '<div class="all__chat__main__class__name">' +
+          '<div class="all__chat__main__class__name__group">' +
+            message.user_name +
+          '</div>' +
+          '<div class="all__chat__main__class__name__info">' +
+          message.date +
+          '</div>' +
+        '</div>' +
+        '<div class="all__chat__main__class__message">' +
+          '<p class="lower-message__content">' +
+            content +
+          '</p>' +
+          '<img src="' + img + '" class="all__chat__main__class__message" >' +
+        '</div>' +
+      '</div>'
+    } else if (content) {
+      //同様に、data-idが反映されるようにしている
+      var html = '<div class="all__chat__main__class" data-id=' + message.id + '>' +
+        '<div class="all__chat__main__class__name">' +
+          '<div class="all__chat__main__class__name__group">' +
+            message.user_name +
+          '</div>' +
+          '<div class="all__chat__main__class__name__info">' +
+            message.date +
+          '</div>' +
+        '</div>' +
+        '<div class="all__chat__main__class__message">' +
+          '<p class="lower-message__content">' +
+            content +
+          '</p>' +
+        '</div>' +
+      '</div>'
+    } else if (img) {
+      //同様に、data-idが反映されるようにしている
+      var html = '<div class="all__chat__main__class" data-id=' + message.id + '>' +
+        '<div class="all__chat__main__class__name">' +
+          '<div class="all__chat__main__class__name__group">' +
+            message.user_name +
+          '</div>' +
+          '<div class="all__chat__main__class__name__info">' +
+            message.date +
+          '</div>' +
+        '</div>' +
+        '<div class="all__chat__main__class__message">' +
+          '<img src="' + img + '" class="all__chat__main__class__message" >' +
+        '</div>' +
+      '</div>'
+    };
+    return html;
+  };
+
+var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  last_message_id = $('.message:last').data('message-id');
+  $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    url: 'api/messages#index {:format=>"json"}',
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    var insertHTML = '';
+    messages.forEach(function(message){
+      insertHTML = buildHTML(message);
+      $('.all__chat__main').append(insertHTML);
+    $('.all__chat__main').animate({ scrollTop: $('.all__chat__main')[0].scrollHeight });
+  })
 })
+
+  .fail(function() {
+    alert("自動更新に失敗しました")
+  })
+}
+setInterval(reloadMessages, 5000);
+});
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,6 +1,10 @@
+json.(@message, :content, :image)
 json.content  @message.content
+json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.user_id  @message.user.id
 json.user_name @message.user.name
-json.group_id  @message.group.id
 json.image  @message.image.url
-json.date  @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.group_id  @message.group.id
+#idもデータとして渡す
+json.id @message.id
+
